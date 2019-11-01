a6eadcda487dfd297f4ab8834a69a483ff37fea6
Brian Nicholls <nichol88@gmail.com>
2019-11-01 05:09:38 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -27,7 +27,7 @@ def third_challenge
         },
       :matriarch => {
         #your key/value pairs here
-        :name => "Lasy Capulet",
+        :name => "Lady Capulet",
         :age => "51"
         },
       :heroine => {
